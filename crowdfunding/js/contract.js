48b4aa5ea9ae5a0594e4f4ad73805b2546211070
MUHAMMAD ALIF BIN KHABALI <1211103373@student.mmu.edu.my>
2026-06-27 20:44:00 +0800

    Final push to main before FE2.0

--- crowdfunding/js/contract.js
+++ crowdfunding/js/contract.js
@@ -3,52 +3,184 @@ const CONTRACT_ADDRESS =
 
 const CONTRACT_ABI = [
     {
-        "inputs": [],
-        "name": "fund",
-        "outputs": [],
-        "stateMutability": "payable",
-        "type": "function"
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_goal",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_durationInDays",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
     },
     {
-        "inputs": [],
-        "name": "withdrawFunds",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
+      "inputs": [],
+      "name": "DeadlineNotPassed",
+      "type": "error"
     },
     {
-        "inputs": [],
-        "name": "claimRefund",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
+      "inputs": [],
+      "name": "DeadlinePassed",
+      "type": "error"
     },
     {
-        "inputs": [],
-        "name": "fundingGoal",
-        "outputs": [{"type":"uint256"}],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [],
+      "name": "GoalNotReached",
+      "type": "error"
     },
     {
-        "inputs": [],
-        "name": "totalFunds",
-        "outputs": [{"type":"uint256"}],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [],
+      "name": "InvalidAmount",
+      "type": "error"
     },
     {
-        "inputs": [],
-        "name": "deadline",
-        "outputs": [{"type":"uint256"}],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [],
+      "name": "TransferFailed",
+      "type": "error"
     },
     {
-        "inputs": [],
-        "name": "goalReached",
-        "outputs": [{"type":"bool"}],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [],
+      "name": "Unauthorized",
+      "type": "error"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "backer",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "message",
+          "type": "string"
+        }
+      ],
+      "name": "Funded",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "creator",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "FundsWithdrawn",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "contributions",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "creator",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "deadline",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_message",
+          "type": "string"
+        }
+      ],
+      "name": "fund",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "goal",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalFunds",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
     }
-];
+  ];
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,3 @@
-// require("@nomicfoundation/hardhat-toolbox");
-// require("dotenv").config();
-
-// module.exports = {
-//   solidity: {
-//     version: "0.8.24",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }
-//   }
-// };
-
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-gas-reporter"); 
 require("dotenv").config();
